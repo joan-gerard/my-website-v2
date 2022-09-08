2337ba8fa12b470964a9f297cd3039391b6ca6a3
Joan Gerard <joan.gerard@outlook.com>
2022-09-08 17:41:29 +0100

    add href to socials

--- client/src/components/SocialMedia.jsx
+++ client/src/components/SocialMedia.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import { AiFillMail } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
+import './SocialMedia.scss';
+
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <FaLinkedinIn />
+      <a href="https://www.linkedin.com/in/joangerard/" className="app__social-link">
+        <FaLinkedinIn />
+      </a>
     </div>
     <div>
-      <FaGithub />
+      <a href="https://github.com/joan-gerard" className="app__social-link">
+        <FaGithub />
+      </a>
     </div>
     <div>
-      <AiFillMail />
+      <a href="mailto:joan-gerard@outlook.com" className="app__social-link">
+        <AiFillMail />
+      </a>
     </div>
   </div>
 );
