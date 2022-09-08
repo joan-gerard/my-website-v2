16673b9c44fcd408af1bf42213040328d860563c
Joan Gerard <joan.gerard@outlook.com>
2022-09-08 16:15:11 +0100

    test deploy

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Development</span> <br />means  <span>Good Business</span></h2>
+      <h2 className="head-text">I Know that <span>Good Development</span> <br />means  <span>Good Business!</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
