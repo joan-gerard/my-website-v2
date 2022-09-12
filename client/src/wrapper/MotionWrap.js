99b0dfa0d1f6e234a28f52ff43da106d49fc799c
Joan Gerard <joan.gerard@outlook.com>
2022-09-12 18:20:08 +0100

    add variant to motion to disable motion on mobile

--- client/src/wrapper/MotionWrap.js
+++ client/src/wrapper/MotionWrap.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const MotionWrap = (Component, classNames) => function HOC() {
+  let boxVariants = {};
+  const isMobile = window.innerWidth < 768; // Add the width you want to check for here (now 768px)
+  if (!isMobile) {
+    boxVariants = {
+      view: {
+        y: [100, 50, 0],
+        opacity: [0, 1],
+      },
+      transition: {
+        duration: 0.6,
+      },
+    };
+  }
+  // if the width >= 768px, boxVariants will be empty, resulting in no animation
+  // you need to refresh the page, it doesn't work when you resize it!
+
   return (
     <motion.div
-      whileInView={{ y: [100, 50, 0], opacity: [0, 1] }}
-      transition={{ duration: 0.6 }}
+      variants={boxVariants}
+      whileInView="view"
+      transition="transition"
       className={`${classNames} app__flex`}
     >
       <Component />
